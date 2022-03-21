145e82f3f53a4a33c9400313fbe6188ba834fb9d
Dekel Cohen <dekelcohen@gmail.com>
2022-03-21 18:48:34 +0200

    minor

--- scripts/run_bert_base_nyt.sh
+++ scripts/run_bert_base_nyt.sh
@@ -1,2 +1,2 @@
 # .bat - set PYTHONPATH=..\..\nlp-utils\src
-export PYTHONPATH=$PYTHONPATH:../../nlp-utils/src; python trainer.py --prepro nyt --data_dir "../Datasets/New York Times Relation Extraction" --model_name_or_path bert-base-cased --input_format typed_entity_marker --seed 23 --train_batch_size 64 --test_batch_size 64 --learning_rate 5e-5 --gradient_accumulation_steps 1 --run_name bert-base
+export PYTHONPATH=$PYTHONPATH:../../nlp-utils/src; python trainer.py --prepro nyt --data_dir "../Datasets/New-York-Times-Relation-Extraction" --model_name_or_path bert-base-cased --input_format typed_entity_marker --seed 23 --train_batch_size 64 --test_batch_size 64 --learning_rate 5e-5 --gradient_accumulation_steps 1 --run_name bert-base
